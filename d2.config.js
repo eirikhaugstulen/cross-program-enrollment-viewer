67c798deecdc6af6ea0bb73e8cc06dedd6881b6f
Eirik Haugstulen <eirik.haugstulen@gmail.com>
2025-11-15 01:45:46 +0300

    fix: release v1.0.1

--- d2.config.js
+++ d2.config.js
@@ -3,7 +3,7 @@ const config = {
     name: 'cross-program-enrollment-viewer',
     title: 'Cross-Program Enrollment Viewer',
     description: 'A plugin that allows users to view and navigate between enrollments across different programs.',
-    version: '1.0.0',
+    version: '1.0.1',
     pluginType: 'TRACKER-ENROLLMENT-PLUGIN',
     
     entryPoints: {
